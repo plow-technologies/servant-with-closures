abf1be7f7d0cf16c6580a2e01aff679656ad7162
Scott Murphy <scottmurphy09@gmail.com>
2015-03-03 21:25:33 -0600

    Closure exported

--- src/ServantClosures/Internal/Closure.hs
+++ src/ServantClosures/Internal/Closure.hs
@@ -1,31 +1,37 @@
-module ServantClosures.Internal.Closure where
+{-# LANGUAGE DeriveDataTypeable #-} -- To enable Typeable on the example type
+{-# LANGUAGE DeriveGeneric #-} -- For the Binary instances on the example
+module ServantClosures.Internal.Closure ( sendComicClosure
+                                        , comicRemoteTable
+                                        , Comic(..)) where
 
-import Data.Text
-import Control.Distributed.Static    
-import Data.Binary       
-import Data.ByteString       
-import Data.Rank1Dynamic (Dynamic, toDynamic, fromDynamic, dynApply)
+import Control.Distributed.Static ( Static
+                                  , staticLabel
+                                  , RemoteTable
+                                  , Closure 
+                                  , staticCompose
+                                  , registerStatic
+                                  , closure
+                                  , initRemoteTable)
+import Data.Binary (Binary
+                   ,encode
+                   ,decode)      
+import GHC.Generics (Generic)      
+import qualified Data.ByteString.Lazy as Lazy 
+import Data.Rank1Dynamic (toDynamic)
 import Data.Rank1Typeable
-  ( Typeable
-  , typeOf
-  , ANY1
-  , ANY2
-  , ANY3
-  , ANY4
-  , isInstanceOf
-  )   
+  ( Typeable)   
   
-  
-data Comic = Comic { title:: Text
-                   , author :: Text }
 
+data Comic = Comic { title:: String
+                   , author :: String }
+ deriving (Typeable , Generic)
 
-
+instance Binary Comic where 
 -- | so for some function that operates on a Comic
 -- to be serialized it first has to be declared static
 
 sendComic :: Comic ->  
-             Text -> 
+             String -> 
              IO Comic
 sendComic c newTitle = return $ c{title=newTitle}
 
@@ -33,16 +39,28 @@ sendComic c newTitle = return $ c{title=newTitle}
 -- |Declare it static 
    
 sendComicStatic :: Static (Comic ->
-                           Text -> IO Comic)
+                           String -> IO Comic)
 sendComicStatic = staticLabel "$sendComic"                           
 
-
-decodeComicStatic :: Static (ByteString -> Int)
+-- |Define a decoder for the serialized free variables
+decodeComicStatic :: Static (Lazy.ByteString -> Comic)
 decodeComicStatic = staticLabel "$decodeComic"
 
-                  
-                  
-rtable :: RemoteTable
-rtable = registerStatic "$sendComic" (toDynamic sendComic)
-       . registerStatic "$decodeComic" (toDynamic (decode :: ByteString -> Comic))
-       $ initRemoteTable               
+
+-- | Define the remote table
+
+comicRemoteTable :: RemoteTable
+comicRemoteTable = registerStatic "$sendComic" (toDynamic sendComic)
+                   . registerStatic "$decodeComic" (toDynamic (decode :: Lazy.ByteString -> Comic))
+                   $ initRemoteTable               
+
+
+-- | define the closure
+-- note... the first argument to the sendComicstatic is decoded by the decodeComicStatic
+-- so it can be furnished by the Serialization   
+sendComicClosure :: Comic -> Closure (String -> IO Comic)
+sendComicClosure c = closure decoder (encode c)
+   where 
+      decoder :: Static (Lazy.ByteString -> String -> IO Comic)
+      decoder = sendComicStatic  `staticCompose` decodeComicStatic
+
